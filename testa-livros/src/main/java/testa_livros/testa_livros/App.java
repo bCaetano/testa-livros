105bc6eab36bf8a0e7f773162725733f9fb3cd5f
Breno Rodrigo <de_breno@hotmail.com>
2017-04-03 08:58:45 -0300

    Adicionando projeto

---
+++ testa-livros/src/main/java/testa_livros/testa_livros/App.java
@@ -0,0 +1,13 @@
+package testa_livros.testa_livros;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
